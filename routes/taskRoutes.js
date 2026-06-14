cdfd074cbc6345f2197b582bc041c46c4aedefe4
Akash Mathew <akashmathewijk@gmail.com>
2026-06-14 19:58:38 +0530

    Add files via upload

---
+++ routes/taskRoutes.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/Task');
+
+
+// GET all tasks
+router.get('/', async (req, res) => {
+
+    const tasks = await Task.find();
+
+    res.json(tasks);
+});
+
+
+// ADD task
+router.post('/', async (req, res) => {
+
+    const task = await Task.create({
+        title: req.body.title
+    });
+
+    res.status(201).json(task);
+});
+
+
+// UPDATE task
+router.put('/:id', async (req, res) => {
+
+    const task = await Task.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.json(task);
+});
+
+
+// DELETE task
+router.delete('/:id', async (req, res) => {
+
+    await Task.findByIdAndDelete(req.params.id);
+
+    res.json({
+        message: 'Task deleted'
+    });
+});
+
+module.exports = router;
