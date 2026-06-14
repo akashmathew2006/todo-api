111bc0a6aab6b9dc5f8a660cd636db035f412cfb
Akash Mathew <akashmathewijk@gmail.com>
2026-06-14 20:00:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-console.log("MONGO_URI =", process.env.MONGO_URI);
-
-mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
